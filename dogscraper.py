60a791e97d00de7c6cee4ddfefb0dd8a7e7b4cbf
Sam Seed <sam.seed@softwire.com>
2021-01-21 23:07:31 +0000

    fix: rename labrador_retriever_id

--- dogscraper.py
+++ dogscraper.py
@@ -22,11 +22,11 @@ def send_email(recipient_email, message):
 english_bulldog_id = 126
 golder_retriever_id = 142
 jack_russell_id = 157
-labrador_retriever = 163
+labrador_retriever_id = 163
 scottish_terrier_id = 208
 yorkshire_terrier_id = 230
 
-URL = f'''https://www.pets4homes.co.uk/search/?type_id=3&breed_id={labrador_retriever}&advert_type=0&results=20&sort=datenew'''
+URL = f'''https://www.pets4homes.co.uk/search/?type_id=3&breed_id={labrador_retriever_id}&advert_type=0&results=20&sort=datenew'''
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 
